f44c27cad1f690d036af02d54411cb4bf5cbd74d
KristoVarul <kvarul@hkhk.edu.ee>
2026-01-24 14:00:25 +0200

    Create Iseseisev töö.py

---
+++ Iseseisev töö.py
@@ -0,0 +1,39 @@
+# Kristo Varul
+# 24.01.2025
+import turtle
+
+# Kujund 5
+
+#t = turtle.Turtle()
+#t.speed(20)
+#t.pensize(2)
+#t.hideturtle()
+#kylje_pikkus = 300
+#
+#for i in range(6):
+#    t.forward(kylje_pikkus)
+#    t.left(120)
+#    t.forward(kylje_pikkus)
+#    t.left(120)
+#    t.forward(kylje_pikkus)
+#    t.left(180)
+#    
+#turtle.done()
+
+# Kujund 9
+
+turtle.speed(0)
+suurus = 50
+for i in range(4):
+    for i in range(3):
+        turtle.forward(suurus)
+        turtle.left(90)
+        turtle.forward(suurus)
+        turtle.right(90)
+        turtle.forward(suurus)
+        turtle.right(90)
+    turtle.forward(suurus)
+    turtle.left(90)
+    turtle.forward(suurus)
+    turtle.right(90)
+    turtle.forward(suurus)
